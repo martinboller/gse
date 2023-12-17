8113a6b199337771160875b1f12fb2e48c90cabf
martinboller <martin@bollers.dk>
2023-12-17 20:10:28 +0100

    outputting inital credentials for gvm

--- installfiles/install-gse.sh
+++ installfiles/install-gse.sh
@@ -1689,6 +1689,12 @@ main() {
     echo -e;
     echo -e "\e[1;32mInstallation complete - Give it a few minutes to complete ingestion of feed data into Postgres/Redis, then reboot\e[0m";
     echo -e "\e[1;32mPrimary Server Install main() finished\e[0m";
+    echo -e;
+    echo -e "\e[1;32m****************************************************************************************************\e[0m";
+    echo -e "\e[1;36m  Initial credential for this Greenbone Community Edition server \e[1;33m$HOSTNAME is";
+    cat /var/lib/gvm/adminuser;
+    echo -e "\e[1;32m****************************************************************************************************\e[0m";
+    echo -e;
     sync; sleep 30; systemctl reboot;
 }
 
